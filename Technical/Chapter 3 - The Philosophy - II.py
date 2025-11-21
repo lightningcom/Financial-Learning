773c2aae3b8329b1191d0003656ea32115e0bd19
lightningcom <lightningcom45@gmail.com>
2025-11-21 22:34:54 +0530

    00

---
+++ Technical/Chapter 3 - The Philosophy - II.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as numpy
+
+st.title("The Philosophy - continued")
+st.write("*We will continue the **The philosophy here***")
+
+st.subheader("Technician or Chartist?", divider=True)
+st.markdown("""
+Well, the term, Chartist is associated with the **act of studying chart**. Charting represents onle one area in the broader subject of technical analysis since study of market action.
+""")
